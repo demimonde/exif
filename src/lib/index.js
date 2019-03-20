1a92c534bdc469b96758f39a6539520861f7ea84
Anton <anton@adc.sh>
2019-03-21 00:05:07 +0300

    unicode characters

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-console */
 const debug = false
 
+// UTF-8 support https://github.com/Philippe64
+// https://github.com/exif-js/exif-js/pull/174
 /**
  * @param {DataView} buffer
  * @param {number} start
@@ -8,9 +10,13 @@ const debug = false
  */
 export function getStringFromDB(buffer, start, length) {
   let outstr = ''
-  for (let n = start; n < start + length; n++) {
-    outstr += String.fromCharCode(buffer.getUint8(n))
+  const arOut = []
+  let j = 0
+  for (let n = start; n < start+length; n++) {
+    arOut[j] = '0x' + buffer.getUint8(n).toString(16)
+    j++
   }
+  outstr = Utf8ArrayToStr(arOut)
   return outstr
 }
 
@@ -129,4 +135,47 @@ function readTagValue(file, entryOffset, tiffStart, bigEnd) {
       return vals
     }
   }
+}
+
+// https://github.com/exif-js/exif-js/pull/174/files
+// adopted from:
+// http://www.onicos.com/staff/iz/amuse/javascript/expert/utf.txt
+/* utf.js - UTF-8 <=> UTF-16 convertion
+ * Copyright (C) 1999 Masanao Izumo <iz@onicos.co.jp>
+ */
+/**
+ * @param {Array<string>} array
+ */
+export const Utf8ArrayToStr = (array) => {
+  var out, i, c
+  var char2, char3
+
+  out = ''
+  const len = array.length
+  i = 0
+  while(i < len) {
+    c = array[i++]
+    switch(c >> 4)
+    {
+    case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7:
+    // 0xxxxxxx
+      out += String.fromCharCode(c)
+      break
+    case 12: case 13:
+    // 110x xxxx   10xx xxxx
+      char2 = array[i++]
+      out += String.fromCharCode(((c & 0x1F) << 6) | (char2 & 0x3F))
+      break
+    case 14:
+    // 1110 xxxx  10xx xxxx  10xx xxxx
+      char2 = array[i++]
+      char3 = array[i++]
+      out += String.fromCharCode(((c & 0x0F) << 12) |
+              ((char2 & 0x3F) << 6) |
+              ((char3 & 0x3F) << 0))
+      break
+    }
+  }
+
+  return out
 }
