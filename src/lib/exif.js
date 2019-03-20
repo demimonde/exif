01840e2fa7e94e9e5f75fc55ba43f8372ea63b3b
Anton <anton@adc.sh>
2019-03-20 16:43:14 +0300

    src

---
+++ src/lib/exif.js
@@ -0,0 +1,285 @@
+/* eslint-disable no-console */
+import readThumbnailImage from './thumbnail'
+import TiffTags from '../tags/tiff'
+import ExifTags from '../tags/exif'
+import GPSTags from '../tags/gps'
+import { getStringFromDB, readTags } from './'
+
+const debug = false
+
+/**
+ * @param {ArrayBuffer} file
+ */
+export function findEXIFinJPEG(file) {
+  var dataView = new DataView(file)
+
+  if (debug) console.log('Got file of length ' + file.byteLength)
+  if ((dataView.getUint8(0) != 0xFF) || (dataView.getUint8(1) != 0xD8)) {
+    if (debug) console.log('Not a valid JPEG')
+    return false // not a valid jpeg
+  }
+
+  var offset = 2,
+    length = file.byteLength,
+    marker
+
+  while (offset < length) {
+    if (dataView.getUint8(offset) != 0xFF) {
+      if (debug) console.log('Not a valid marker at offset ' + offset + ', found: ' + dataView.getUint8(offset))
+      return false // not a valid marker, something is wrong
+    }
+
+    marker = dataView.getUint8(offset + 1)
+    if (debug) console.log(marker)
+
+    // we could implement handling for other markers here,
+    // but we're only looking for 0xFFE1 for EXIF data
+
+    if (marker == 225) {
+      if (debug) console.log('Found 0xFFE1 marker')
+
+      return readEXIFData(dataView, offset + 4, dataView.getUint16(offset + 2) - 2)
+
+      // offset += 2 + file.getShortAt(offset+2, true);
+    } else {
+      offset += 2 + dataView.getUint16(offset+2)
+    }
+  }
+}
+
+/**
+ * @param {DataView} file
+ * @param {number} start
+ */
+function readEXIFData(file, start) {
+  if (getStringFromDB(file, start, 4) != 'Exif') {
+    if (debug) console.log('Not valid EXIF data! ' + getStringFromDB(file, start, 4))
+    return false
+  }
+
+  var bigEnd,
+    tags, tag,
+    exifData, gpsData,
+    tiffOffset = start + 6
+
+  // test for TIFF validity and endianness
+  if (file.getUint16(tiffOffset) == 0x4949) {
+    bigEnd = false
+  } else if (file.getUint16(tiffOffset) == 0x4D4D) {
+    bigEnd = true
+  } else {
+    if (debug) console.log('Not valid TIFF data! (no 0x4949 or 0x4D4D)')
+    return false
+  }
+
+  if (file.getUint16(tiffOffset+2, !bigEnd) != 0x002A) {
+    if (debug) console.log('Not valid TIFF data! (no 0x002A)')
+    return false
+  }
+
+  var firstIFDOffset = file.getUint32(tiffOffset+4, !bigEnd)
+
+  if (firstIFDOffset < 0x00000008) {
+    if (debug) console.log('Not valid TIFF data! (First offset less than 8)', file.getUint32(tiffOffset+4, !bigEnd))
+    return false
+  }
+
+  tags = readTags(file, tiffOffset, tiffOffset + firstIFDOffset, TiffTags, bigEnd)
+
+  if (tags.ExifIFDPointer) {
+    exifData = readTags(file, tiffOffset, tiffOffset + tags.ExifIFDPointer, ExifTags, bigEnd)
+    for (tag in exifData) {
+      switch (tag) {
+      case 'LightSource' :
+      case 'Flash' :
+      case 'MeteringMode' :
+      case 'ExposureProgram' :
+      case 'SensingMethod' :
+      case 'SceneCaptureType' :
+      case 'SceneType' :
+      case 'CustomRendered' :
+      case 'WhiteBalance' :
+      case 'GainControl' :
+      case 'Contrast' :
+      case 'Saturation' :
+      case 'Sharpness' :
+      case 'SubjectDistanceRange' :
+      case 'FileSource' :
+        exifData[tag] = StringValues[tag][exifData[tag]]
+        break
+
+      case 'ExifVersion' :
+      case 'FlashpixVersion' :
+        exifData[tag] = String.fromCharCode(exifData[tag][0], exifData[tag][1], exifData[tag][2], exifData[tag][3])
+        break
+
+      case 'ComponentsConfiguration' :
+        exifData[tag] =
+                        StringValues.Components[exifData[tag][0]] +
+                        StringValues.Components[exifData[tag][1]] +
+                        StringValues.Components[exifData[tag][2]] +
+                        StringValues.Components[exifData[tag][3]]
+        break
+      }
+      tags[tag] = exifData[tag]
+    }
+  }
+
+  if (tags.GPSInfoIFDPointer) {
+    gpsData = readTags(file, tiffOffset, tiffOffset + tags.GPSInfoIFDPointer, GPSTags, bigEnd)
+    for (tag in gpsData) {
+      switch (tag) {
+      case 'GPSVersionID' :
+        gpsData[tag] = gpsData[tag][0] +
+                        '.' + gpsData[tag][1] +
+                        '.' + gpsData[tag][2] +
+                        '.' + gpsData[tag][3]
+        break
+      }
+      tags[tag] = gpsData[tag]
+    }
+  }
+
+  // extract thumbnail
+  tags['thumbnail'] = readThumbnailImage(file, tiffOffset, firstIFDOffset, bigEnd)
+
+  return tags
+}
+
+const StringValues = {
+  ExposureProgram: {
+    0: 'Not defined',
+    1: 'Manual',
+    2: 'Normal program',
+    3: 'Aperture priority',
+    4: 'Shutter priority',
+    5: 'Creative program',
+    6: 'Action program',
+    7: 'Portrait mode',
+    8: 'Landscape mode',
+  },
+  MeteringMode: {
+    0: 'Unknown',
+    1: 'Average',
+    2: 'CenterWeightedAverage',
+    3: 'Spot',
+    4: 'MultiSpot',
+    5: 'Pattern',
+    6: 'Partial',
+    255: 'Other',
+  },
+  LightSource: {
+    0: 'Unknown',
+    1: 'Daylight',
+    2: 'Fluorescent',
+    3: 'Tungsten (incandescent light)',
+    4: 'Flash',
+    9: 'Fine weather',
+    10: 'Cloudy weather',
+    11: 'Shade',
+    12: 'Daylight fluorescent (D 5700 - 7100K)',
+    13: 'Day white fluorescent (N 4600 - 5400K)',
+    14: 'Cool white fluorescent (W 3900 - 4500K)',
+    15: 'White fluorescent (WW 3200 - 3700K)',
+    17: 'Standard light A',
+    18: 'Standard light B',
+    19: 'Standard light C',
+    20: 'D55',
+    21: 'D65',
+    22: 'D75',
+    23: 'D50',
+    24: 'ISO studio tungsten',
+    255: 'Other',
+  },
+  Flash: {
+    0x0000: 'Flash did not fire',
+    0x0001: 'Flash fired',
+    0x0005: 'Strobe return light not detected',
+    0x0007: 'Strobe return light detected',
+    0x0009: 'Flash fired, compulsory flash mode',
+    0x000D: 'Flash fired, compulsory flash mode, return light not detected',
+    0x000F: 'Flash fired, compulsory flash mode, return light detected',
+    0x0010: 'Flash did not fire, compulsory flash mode',
+    0x0018: 'Flash did not fire, auto mode',
+    0x0019: 'Flash fired, auto mode',
+    0x001D: 'Flash fired, auto mode, return light not detected',
+    0x001F: 'Flash fired, auto mode, return light detected',
+    0x0020: 'No flash function',
+    0x0041: 'Flash fired, red-eye reduction mode',
+    0x0045: 'Flash fired, red-eye reduction mode, return light not detected',
+    0x0047: 'Flash fired, red-eye reduction mode, return light detected',
+    0x0049: 'Flash fired, compulsory flash mode, red-eye reduction mode',
+    0x004D: 'Flash fired, compulsory flash mode, red-eye reduction mode, return light not detected',
+    0x004F: 'Flash fired, compulsory flash mode, red-eye reduction mode, return light detected',
+    0x0059: 'Flash fired, auto mode, red-eye reduction mode',
+    0x005D: 'Flash fired, auto mode, return light not detected, red-eye reduction mode',
+    0x005F: 'Flash fired, auto mode, return light detected, red-eye reduction mode',
+  },
+  SensingMethod: {
+    1: 'Not defined',
+    2: 'One-chip color area sensor',
+    3: 'Two-chip color area sensor',
+    4: 'Three-chip color area sensor',
+    5: 'Color sequential area sensor',
+    7: 'Trilinear sensor',
+    8: 'Color sequential linear sensor',
+  },
+  SceneCaptureType: {
+    0: 'Standard',
+    1: 'Landscape',
+    2: 'Portrait',
+    3: 'Night scene',
+  },
+  SceneType: {
+    1: 'Directly photographed',
+  },
+  CustomRendered: {
+    0: 'Normal process',
+    1: 'Custom process',
+  },
+  WhiteBalance: {
+    0: 'Auto white balance',
+    1: 'Manual white balance',
+  },
+  GainControl: {
+    0: 'None',
+    1: 'Low gain up',
+    2: 'High gain up',
+    3: 'Low gain down',
+    4: 'High gain down',
+  },
+  Contrast: {
+    0: 'Normal',
+    1: 'Soft',
+    2: 'Hard',
+  },
+  Saturation: {
+    0: 'Normal',
+    1: 'Low saturation',
+    2: 'High saturation',
+  },
+  Sharpness: {
+    0: 'Normal',
+    1: 'Soft',
+    2: 'Hard',
+  },
+  SubjectDistanceRange: {
+    0: 'Unknown',
+    1: 'Macro',
+    2: 'Close view',
+    3: 'Distant view',
+  },
+  FileSource: {
+    3: 'DSC',
+  },
+
+  Components: {
+    0: '',
+    1: 'Y',
+    2: 'Cb',
+    3: 'Cr',
+    4: 'R',
+    5: 'G',
+    6: 'B',
+  },
+}
---
+++ src/lib/iptc.js
@@ -0,0 +1,97 @@
+/* eslint-disable no-console */
+import { getStringFromDB } from './'
+
+const debug = false
+
+/**
+ * @param {ArrayBuffer} file
+ */
+export default function findIPTCinJPEG(file) {
+  const dataView = new DataView(file)
+
+  if (debug) console.log('Got file of length %s', file.byteLength)
+  if ((dataView.getUint8(0) != 0xFF) || (dataView.getUint8(1) != 0xD8)) {
+    if (debug) console.log('Not a valid JPEG')
+    return false // not a valid jpeg
+  }
+
+  let offset = 2,
+    length = file.byteLength
+
+  while (offset < length) {
+    if (isFieldSegmentStart(dataView, offset )){
+      // Get the length of the name header (which is padded to an even number of bytes)
+      var nameHeaderLength = dataView.getUint8(offset+7)
+      if(nameHeaderLength % 2 !== 0) nameHeaderLength += 1
+      // Check for pre photoshop 6 format
+      if(nameHeaderLength === 0) {
+        // Always 4
+        nameHeaderLength = 4
+      }
+
+      var startOffset = offset + 8 + nameHeaderLength
+      var sectionLength = dataView.getUint16(offset + 6 + nameHeaderLength)
+
+      return readIPTCData(file, startOffset, sectionLength)
+    }
+
+    // Not the marker, continue searching
+    offset++
+  }
+}
+
+const IptcFieldMap = {
+  0x78: 'caption',
+  0x6E: 'credit',
+  0x19: 'keywords',
+  0x37: 'dateCreated',
+  0x50: 'byline',
+  0x55: 'bylineTitle',
+  0x7A: 'captionWriter',
+  0x69: 'headline',
+  0x74: 'copyright',
+  0x0F: 'category',
+}
+
+function readIPTCData(file, startOffset, sectionLength){
+  const dataView = new DataView(file)
+  var data = {}
+  let fieldValue, fieldName, dataSize, segmentType
+  let segmentStartPos = startOffset
+  while(segmentStartPos < startOffset + sectionLength) {
+    if(dataView.getUint8(segmentStartPos) === 0x1C && dataView.getUint8(segmentStartPos+1) === 0x02){
+      segmentType = dataView.getUint8(segmentStartPos+2)
+      if(segmentType in IptcFieldMap) {
+        dataSize = dataView.getInt16(segmentStartPos+3)
+        fieldName = IptcFieldMap[segmentType]
+        fieldValue = getStringFromDB(dataView, segmentStartPos+5, dataSize)
+        // Check if we already stored a value with this name
+        if(data.hasOwnProperty(fieldName)) {
+          // Value already stored with this name, create multivalue field
+          if(data[fieldName] instanceof Array) {
+            data[fieldName].push(fieldValue)
+          }
+          else {
+            data[fieldName] = [data[fieldName], fieldValue]
+          }
+        }
+        else {
+          data[fieldName] = fieldValue
+        }
+      }
+    }
+    segmentStartPos++
+  }
+  return data
+}
+
+function isFieldSegmentStart(dataView, offset){
+  return (
+    dataView.getUint8(offset) === 0x38 &&
+    dataView.getUint8(offset+1) === 0x42 &&
+    dataView.getUint8(offset+2) === 0x49 &&
+    dataView.getUint8(offset+3) === 0x4D &&
+    dataView.getUint8(offset+4) === 0x04 &&
+    dataView.getUint8(offset+5) === 0x04
+  )
+}
